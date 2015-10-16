cb5a606c21851c252416f8531de9c56fa590e34c
mercieje <mercieje@PC-B101-02.iut-valence.fr>
2015-10-16 15:49:43 +0200

    deleting useless javadoc

--- iutvalence-projetS2-g17-photop-master/src/fr/iutvalence/info/projet/s2/g17/photop/PhotopObject.java
+++ iutvalence-projetS2-g17-photop-master/src/fr/iutvalence/info/projet/s2/g17/photop/PhotopObject.java
@@ -7,14 +7,9 @@ package fr.iutvalence.info.projet.s2.g17.photop;
  */
 public abstract class PhotopObject
 {
-	/**
-	 * the coordinate of the object
-	 */
+
 	private Position objectPosition;
 	
-	/**
-	 * the dimension of the object
-	 */
 	private int objectDimension;
 	
 	/**
@@ -28,37 +23,21 @@ public abstract class PhotopObject
 		this.objectDimension = objectDimension;
 	}
 	
-	/**
-	 * gets the object's position
-	 * @return objectPosition the object's position
-	 */
 	public Position getObjectPosition()
 	{
 		return objectPosition;
 	}
 	
-	/**
-	 * Sets a new position to the object
-	 * @param objectPosition objectPosition
-	 */
 	public void setObjectPosition(Position objectPosition)
 	{
 		this.objectPosition = objectPosition;
 	}
 	
-	/**
-	 * gets the dimension of the object
-	 * @return objectDimension objectDimension
-	 */
 	public int getObjectDimension()
 	{
 		return objectDimension;
 	}
 	
-	/**
-	 * sets the new dimension to the object
-	 * @param objectDimension objectDimension
-	 */
 	public void setObjectDimension(int objectDimension)
 	{
 		this.objectDimension = objectDimension;
