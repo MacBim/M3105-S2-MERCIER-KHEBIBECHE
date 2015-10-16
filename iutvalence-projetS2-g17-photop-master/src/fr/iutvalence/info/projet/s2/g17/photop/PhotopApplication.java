f8baafbdc6b8c9b97cc4095fb098c8f6880559ed
mercieje <mercieje@PC-B101-02.iut-valence.fr>
2015-10-16 16:06:23 +0200

    Rename / deleting useless javadoc of PhotopApplication

---
+++ iutvalence-projetS2-g17-photop-master/src/fr/iutvalence/info/projet/s2/g17/photop/PhotopApplication.java
@@ -0,0 +1,42 @@
+package fr.iutvalence.info.projet.s2.g17.photop;
+
+import fr.iutvalence.info.projet.s2.g17.photop.GUI.GUIPhotop;
+
+
+/**
+ * Photop' is a photo editing software
+ * Features :
+ *  <ul>
+ *  	<li>open an image</li>
+ *  	<li>create an image</li>
+ *  	<li>save an image</li>
+ *    	<li>transform an image</li>
+ *  	<li>add shapes</li>
+ *  	<li>add text</li>
+ *   	<li>add embellishment</li>
+ *     	<li>add a frame</li>
+ *     	<li>draw</li>
+ *  </ul>
+ *
+ */
+
+public class PhotopApplication 
+{
+
+	private GUIPhotop guiPhotop;
+	
+	/**
+	 * The association of the GUI and the application
+	 * @param guiPhotop guiPhotop
+	 */
+	public void GUIAssociation(GUIPhotop guiPhotop)
+	{
+		this.guiPhotop = guiPhotop;
+		
+	}
+	
+	
+	
+	
+	
+}
